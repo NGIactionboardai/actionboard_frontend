762b5427b12a8912b7f4dd2a95263cfaf4a51112
Johurul Hassan Mazumder <johurulhassan000@gmail.com>
2025-07-18 04:14:01 +0530

    Updated the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,26 @@ module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    'bg-overlay-dark',
+    'from-btn-from',
+    'to-btn-to',
+    'hover:from-btn-hover-from',
+    'hover:to-btn-hover-to',
+  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter", ...fontFamily.sans],
+      },
+      backgroundColor: {
+        'overlay-dark': 'rgba(0, 0, 0, 0.3)',
+      },
+      colors: {
+        'btn-from': '#0A0DC4',
+        'btn-to': '#8B0782',
+        'btn-hover-from': '#080aa8',
+        'btn-hover-to': '#6d0668',
       },
     },
   },
