1928da600d8d45b1a437af09e6ed790c1af345cd
Johurul Hassan Mazumder <johurulhassan000@gmail.com>
2025-08-06 13:00:47 +0530

    updated the navbar

--- src/app/components/layout/Navbar.js
+++ src/app/components/layout/Navbar.js
@@ -187,7 +187,13 @@ export default function Navbar() {
                     {orgsOpen && (
                       <div className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50">
                         <div className="py-1 text-sm text-gray-700">
-
+                            <Link
+                              href="/organizations"
+                              className="block px-4 py-2 hover:bg-gray-100"
+                              onClick={() => setOrgsOpen(false)}
+                            >
+                              Personal
+                            </Link>
                           {orgs.map((org) => (
                             <Link
                               key={org.org_id}
